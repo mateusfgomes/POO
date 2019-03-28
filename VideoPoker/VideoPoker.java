ee2290aeec8e287d10b282ca7c70d820be2663f7
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2019-03-28 08:33:21 -0300

    Add files via upload

--- VideoPoker/VideoPoker.java
+++ VideoPoker/VideoPoker.java
@@ -1,7 +1,6 @@
-
 /**
  * Jogo de VideoPoker desenvolvido para a aula de Programacao Orientada a Objetos 
- * @author Mateus Ferreira Gomes
+ * @author Mateus Ferreira Gomes e Guilherme Targon Marques Barcellos
  *
  */
 public class VideoPoker {	
@@ -9,26 +8,46 @@ public class VideoPoker {
 	public static void main(String[] args) throws java.io.IOException{
 		
 		String mostra_jogadas, inicia;
-		String escolha, placar_agora;
+		String escolha; 
 		String ans;
-		int pos;
-		int pontos;
 		int aposta;
 		int[][] resultado = new int[5][2];
 		SorteiaCartas c = new SorteiaCartas();
-		Fichas f = new Fichas();
+		Dealer f = new Dealer();
 		
 		System.out.println("Bem vindo ao VideoPoker\n\n");
 		
 		
 		while(f.getSaldo() > 0){
-			System.out.println("Pressione ENTER para iniciar a rodada\n");
+			System.out.println("Pressione ENTER para iniciar a rodada ou 'E' para finalizar\n");
 			
 			inicia = EntradaTeclado.leString();
-			System.out.println("Aposta\n");
-			aposta = EntradaTeclado.leInt();
+			if(inicia.contentEquals("e") || inicia.contentEquals("E")){
+				System.out.println("Finalizando VideoPoker...\n");
+				System.out.println("Seu saldo final fichas: "+f.getSaldo()+"\n");
+				System.out.println("VideoPoker finalizado. Obrigado por jogar!\n");
+				return;
+			}
+			System.out.println("Digite valor da aposta\n");
+			
+			while(true){
+				try{
+					aposta = EntradaTeclado.leInt();
+					while(aposta <0 || aposta > f.getSaldo()){
+						System.out.println("Valor da aposta nao eh valido, digite valor maior que zero e menor que seu saldo fichas\n");
+						aposta = EntradaTeclado.leInt();
+					}
+					break;
+				}
+				catch (Exception e){
+					System.out.println("Valor da aposta nao eh valido, digite um numero inteiro\n");
+					continue;
+				}
+				
+			}
+			
 			f.setAposta(aposta);
-			if(inicia != null && aposta > 0 && aposta <= f.getSaldo()) {
+			if(inicia != null ) {
 				resultado = c.sorteia("1 2 3 4 5");
 				mostra_jogadas = c.toString();
 				System.out.println(mostra_jogadas);
@@ -40,25 +59,14 @@ public class VideoPoker {
 				resultado = c.sorteia(escolha);
 				mostra_jogadas = c.toString();
 				System.out.println(mostra_jogadas);
-				/*resultado[0][0] = 0;
-				resultado[1][0] = 1;
-				resultado[2][0] = 2;
-				resultado[3][0] = 3;
-				resultado[4][0] = 0;
-				
-				resultado[0][1] = 10;
-				resultado[1][1] = 10;
-				resultado[2][1] = 2;
-				resultado[3][1] = 2;
-				resultado[4][1] = 1;
-				*/
 				ans = f.verificaCombinacoes(resultado);
 				System.out.println(ans);
-				System.out.println(f.getSaldo());
+				System.out.println("Saldo atual: "+f.getSaldo());
+				c.resetaUsadas();
 			}
 			
 		}
-		
+		System.out.println("Fim de jogo!!!!");
 	}
 	
 }
