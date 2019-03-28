18c018b03b4a7fb5673f2e2e2fb2866437ad7b3c
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2019-03-28 08:33:21 -0300

    Changed files via upload

--- VideoPoker/SorteiaCartas.java
+++ VideoPoker/SorteiaCartas.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
 
+/**
+ * Classe Sorteia Cartas, que tem a funcao de sortear as cartas usadas para jogar e marcar
+ * as cartas a fim de nao repeti-las
+ * @author Mateus Ferreira Gomes e Guilherme Targon Marques Barcellos
+ *
+ */
 public class SorteiaCartas {
 	
 	Carta[] c;
+	public String a;
+	boolean[] cartas_usadas = new boolean[53];
 	
+	
+	/**
+	 * Construtor para criar um vetor de cartas a fim de jogar com elas
+	 */
 	public SorteiaCartas() {
 		c = new Carta[5];
 		for(int i = 0; i < 5; i++) {
@@ -10,35 +23,42 @@ public class SorteiaCartas {
 		}
 	}
 	
+	/**
+	 * Metodo que realiza o sorteio das cartas sempre a partir de uma string
+	 * @param s string que indica quais cartas serao trocadas
+	 * @return vetor de inteiros de cartas resultado da troca
+	 */
 	public int[][] sorteia(String s) {
-		
-		int[][] faces_resultado = new int[5][2];
-		
-		for(int i = 0; i < s.length(); i++) {
-			int posicao = s.charAt(i) -'0';
-			if(posicao > 0 && posicao < 6) {
-				Carta teste = new Carta();
-				teste.aleatorizarCarta();
-				for(int j = 0; j < 5; j++) {
-					while(teste == c[j]) {
-						teste.aleatorizarCarta();
-					}
+			
+			int[][] faces_resultado = new int[5][2];
+			
+			for(int i = 0; i < s.length(); i++) {
+				int posicao = s.charAt(i) -'0';
+				if(posicao > 0 && posicao < 6) {
+					Carta teste = new Carta();
+					teste.aleatorizarCarta();
+					int pos_bool = (teste.getFace()[0])*13 + teste.getFace()[1];
+						while(cartas_usadas[pos_bool]) {
+							teste.aleatorizarCarta();
+							pos_bool = (teste.getFace()[0])*13 + teste.getFace()[1];
+						}
+					cartas_usadas[pos_bool] = true;
+					c[posicao-1] = teste;	
 				}
-				c[posicao-1] = teste;
-				
 			}
-		}
-		
-		for(int j = 0; j < 5; j++) {
-			faces_resultado[j] = c[j].getFace();
-		}
-		
-		return faces_resultado;
-		 
+			
+			for(int j = 0; j < 5; j++) {
+				faces_resultado[j] = c[j].getFace();
+			}
+			
+			return faces_resultado;
+			 
 	}
 	
 
-	
+	/**
+	 * Metodo toString(), o qual tem a funcao de mostrar literalmente as cartas
+	 */
 	public String toString() {
 		
 		String s = "";
@@ -117,6 +137,13 @@ public class SorteiaCartas {
 		
 		return s;
 		
+	}
+	
+	/**
+	 * Metodo que reseta o vetor que marca as cartas usadas
+	 */
+	public void resetaUsadas() {
+		Arrays.fill(cartas_usadas, false);
 	}
 	
 }
