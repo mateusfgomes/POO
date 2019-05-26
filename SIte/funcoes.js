4630b0b73d5c38a81f86a0f53a614c6c035b9d00
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2019-05-26 01:27:10 -0300

    Add files via upload

---
+++ SIte/funcoes.js
@@ -0,0 +1,26 @@
+function concluido() {
+    var nome = document.getElementById("nome");
+    var valor_nome = nome.value;
+    var telefone = document.getElementById("telefone");
+    var valor_telefone = telefone.value;
+    var cpf = document.getElementById("cpf");
+    var valor_cpf = cpf.value;
+    var rg = document.getElementById("rg");
+    var valor_rg = rg.value;
+    var test = confirm(valor_nome + "\n" + valor_telefone + "\n" + valor_cpf + "\n" + valor_rg);
+
+    if(test == true){
+        window.location.href = "./finalizado.html";
+    }
+    else{
+        
+    }
+}
+
+function fazerDoacao(){
+
+}
+
+function cadastroIndividual(){
+    window.location.href = "./cadastro_i.html";
+}
